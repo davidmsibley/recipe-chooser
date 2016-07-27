b602ed4d67c5d66d748958f9ca8f964de196956f
David M Sibley <david.m.sibley@gmail.com>
2016-07-27 13:08:46 -0500

    babby's first vue

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,29 @@
 <template>
-  <div id="app">
-    <h1>{{ msg }}</h1>
-  </div>
+    <div id="app">
+        <template v-for="ingredient in ingredients">
+            <ingredient :ingredient="ingredient"></ingredient>
+        </template>
+    </div>
 </template>
 
 <script>
+import Ingredient from './Ingredient.vue'
+import Ingredients from './../generatedData/ingredientOrder.json'
+
 export default {
-  data () {
-    return {
-      // note: changing this line won't causes changes
-      // with hot-reload because the reloaded component
-      // preserves its current state and we are modifying
-      // its initial state.
-      msg: 'Hello Vue!'
+    data () {
+        return {
+            ingredients: Ingredients
+        }
+    },
+    components: {
+        Ingredient
     }
-  }
 }
 </script>
 
 <style>
 body {
-  font-family: Helvetica, sans-serif;
+    font-family: Helvetica, sans-serif;
 }
 </style>
---
+++ src/Ingredient.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="ingredient">
+        <span class="ingredient-name">{{ ingredient.name }}</span>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['ingredient']
+}
+</script>
+
+<style scoped>
+body {
+    font-family: Helvetica, sans-serif;
+}
+</style>
