91e8975c1ccae5a752d7584f95578cc1ff570810
David M Sibley <david.m.sibley@gmail.com>
2016-07-26 19:26:05 -0500

    mapped to multiple entries

--- scraper.js
+++ scraper.js
@@ -1,5 +1,6 @@
 let jsdom = require("jsdom");
-let promisify = require('bluebird').promisify;
+let bluebird = require("bluebird");
+let promisify = bluebird.promisify;
 let dom = promisify(jsdom.env);
 
 function clean ($this) {
@@ -36,51 +37,57 @@ function parseIngredient (fullStr) {
     };
 };
 
-let i = 430;
+let i;
+let djambi = [];
 
-dom(__dirname + "/downloaded/" + i + ".html", ["http://code.jquery.com/jquery.js"])
-    .then(function (window) {
-        let result = {};
-        let $ = window.$;
+for (i = 430; i < 432; i++) {
+    djambi.push(dom(__dirname + "/downloaded/" + i + ".html", ["http://code.jquery.com/jquery.js"]));
+}
 
-        result.id = i;
+bluebird.map(djambi, function scrapeAndButterTheOrgyPit (window) {
+    let result = {};
+    let $ = window.$;
 
-        $("meta[property='og:title']").each(function() {
-            result.title = $(this).attr("content");
-        });
+    result.id = window.location.href.split("/").slice(-1)[0].split(".")[0];
 
-        $("meta[property='og:description']").each(function() {
-            result.description = $(this).attr("content");
-        });
-
-        $("span[itemprop='calories']").each(function() {
-            result.calories = clean($(this));
-        });
-
-        $("img.rec-splash-img").each(function() {
-            result.splash = $(this).attr("src");
-        });
-
-        result.ingredients = [];
-        $("li[itemprop='ingredients']").each(function() {
-            result.ingredients.push(parseIngredient(clean($(this))));
-        });
-
-        result.steps = [];
-        $("#instructions .instr-step").each(function() {
-            let $this = $(this);
-            let step = clean($this.find(".instr-num"));
-            let title = clean($this.find(".instr-title"));
-            let img = $this.find(".img-max").attr("src");
-            let text = clean($this.find(".instr-txt"));
-
-            result.steps.push({
-                step,
-                title,
-                img,
-                text
-            });
-        });
-
-        console.log(result);
+    $("meta[property='og:title']").each(function() {
+        result.title = $(this).attr("content");
     });
+
+    $("meta[property='og:description']").each(function() {
+        result.description = $(this).attr("content");
+    });
+
+    $("span[itemprop='calories']").each(function() {
+        result.calories = clean($(this));
+    });
+
+    $("img.rec-splash-img").each(function() {
+        result.splash = $(this).attr("src");
+    });
+
+    result.ingredients = [];
+    $("li[itemprop='ingredients']").each(function() {
+        result.ingredients.push(parseIngredient(clean($(this))));
+    });
+
+    result.steps = [];
+    $("#instructions .instr-step").each(function() {
+        let $this = $(this);
+        let step = clean($this.find(".instr-num"));
+        let title = clean($this.find(".instr-title"));
+        let img = $this.find(".img-max").attr("src");
+        let text = clean($this.find(".instr-txt"));
+
+        result.steps.push({
+            step,
+            title,
+            img,
+            text
+        });
+    });
+
+    return result;
+}).then(function(done) {
+    console.log(JSON.stringify(done));
+});
