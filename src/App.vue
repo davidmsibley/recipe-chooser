899c67bb58119141a51eaa9c650df7c5a19e32f6
David M Sibley <david.m.sibley@gmail.com>
2016-07-27 16:22:56 -0500

    initial drafting of layout

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,34 @@
 <template>
-    <div id="app">
-        <template v-for="ingredient in ingredients">
-            <ingredient :ingredient="ingredient"></ingredient>
-        </template>
+    <div id="app" class="container center-align flow-text">
+        <div class="row">
+            <div class="view-title col s12">
+                <h3>Ingredients</h3>
+            </div>
+            <div class="col s2">
+                <div class="card-panel green lighten-2">Vegetables</div>
+            </div>
+            <div class="col s2">
+                <div class="card-panel red lighten-2">Meat</div>
+            </div>
+            <div class="col s2">
+                <div class="card-panel amber lighten-2">Starches</div>
+            </div>
+            <div class="col s2">
+                <div class="card-panel purple lighten-2">Sauces</div>
+            </div>
+            <div class="col s2">
+                <div class="card-panel lime lighten-2">Fruit</div>
+            </div>
+            <div class="col s2">
+                <div class="card-panel brown lighten-2">Spices</div>
+            </div>
+        </div>
+        <div class="row divider"></div>
+        <div class="row">
+            <template v-for="ingredient in ingredients">
+                <ingredient :ingredient="ingredient"></ingredient>
+            </template>
+        </div>
     </div>
 </template>
 
@@ -24,7 +50,7 @@ export default {
 </script>
 
 <style>
-body {
-    font-family: Helvetica, sans-serif;
-}
+    #app {
+
+    }
 </style>
--- src/Ingredient.vue
+++ src/Ingredient.vue
@@ -1,17 +1,20 @@
 <template>
-    <div class="ingredient">
-        <span class="ingredient-name">{{ ingredient.name }}</span>
+    <div class="ingredient col s6 m4 l3">
+        <div class="card-panel cyan lighten-2 hoverable">
+            <div class="ingredient-thumb"></div>
+            <div class="ingredient-name white-text">{{ ingredient.name }}</div>
+            <div class="ingredient-detail"></div>
+        </div>
     </div>
 </template>
 
 <script>
+//TODO pull the card styling up!
 export default {
     props: ['ingredient']
 }
 </script>
 
 <style scoped>
-body {
-    font-family: Helvetica, sans-serif;
-}
+
 </style>
