c81599700d2e76a78e3fb85b90676a9c9e501b5c
David M Sibley <david.m.sibley@gmail.com>
2016-08-01 12:22:59 -0500

    using a connection pool with request, which I should have done from the start

--- downloader.js
+++ downloader.js
@@ -1,12 +1,20 @@
 var request = require('request');
 var fs = require('fs');
 
+limitedRequest = request.defaults({ pool : {maxSockets : 10} });
 var download = function(url, dest) {
     var file = fs.createWriteStream(dest);
-    request(url).pipe(file);
+    limitedRequest(url)
+    .on('response', function(response) {
+        console.log(response.statusCode, url)
+    })
+    .on('error', function(error) {
+        console.log(error)
+    })
+    .pipe(file);
 };
 
 var i;
-for (i = 430; i < 950; i++ ) {
+for (i = 450; i < 950; i++ ) {
     download('https://www.blueapron.com/recipes/' + i, __dirname + '/downloaded/'+ i +'.html');
 }
